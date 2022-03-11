cd69a10523a21260d3be75a54a0ea82a0d90678a
Kamil Klekotka <kamil.klekotka@gmail.com>
2022-03-11 16:53:27 +0100

    working cron

--- BankScrap/BankScrap/settings.py
+++ BankScrap/BankScrap/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_crontab',
 
     # local
     'scraping.apps.ScrapingConfig',
@@ -126,3 +127,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CronJobs
+
+CRONJOBS = [
+    ('*/1 * * * *', 'scraping.cron.my_scheduled_job')
+]
