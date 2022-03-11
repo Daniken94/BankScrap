bbd50d9c4aa38865024ee5cc1689d1bf4e84113e
Kamil Klekotka <kamil.klekotka@gmail.com>
2022-03-11 12:48:23 +0100

    finish scrap function

--- BankScrap/scraping/connecting.py
+++ BankScrap/scraping/connecting.py
@@ -16,29 +16,22 @@ for i in range(len(name)):
     new.append(df3)
     j = j+1
 
-sql = """INSERT INTO scraping_stats(name, code, price, date) VALUES(?,?,?,?)"""
+# sql = """INSERT INTO scraping_stats(name, code, price, date) VALUES(?,?,?,?)"""
 
-cur.executemany(sql, new)
-conn.commit()
-print("complete")
-
-# def data_entry():
-#     for item in id:
-#         cur.execute("INSERT INTO scraping_stats VALUES(?,?,?,?,?)", item)
-
-
-# one = [1, 2, 3, 4]
-# two = ["one", "two", "three", "four"]
-# three = ["ja", "to", "jakoś", "pochytom"]
-
-# df4 = one[3], two[3], three[3]
-# print(df4)
-
-
-
-
-# cur.execute('''INSERT INTO scraping_stats VALUES(?,?,?,?,?)''',(id, name, code, price, date))
+# cur.executemany(sql, new)
 # conn.commit()
+# print("complete")
+
+cur.execute("DELETE FROM scraping_stats;")
+print('We have deleted', cur.rowcount, 'records from the table.')
+conn.commit()
+
+
+
+
+
+
+
 
 
 cur.execute('''SELECT * FROM scraping_stats''')
@@ -46,10 +39,4 @@ results = cur.fetchall()
 
 print(results)
 
-# table_rows = [id, name, code, price, date]
-# sql = '''INSERT INTO scraping_stats VALUES(?,?,?,?,?) '''
-
-# cur.executemany(sql, table_rows)
-
-# for row in cur.execute('SELECT * FROM scraping_stats'):
-#     print(row)
+conn.close()
